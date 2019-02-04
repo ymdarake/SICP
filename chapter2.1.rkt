bd3192056b11c07d8a865fd3d419b7c743e1a5a9
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2019-02-05 00:27:12 +0900

    [add]2.7 - 2.10

--- chapter2.1.rkt
+++ chapter2.1.rkt
@@ -145,5 +145,43 @@
   (lambda (f) (lambda (x) (f (f x)))))
 
 
+; Exercise 2.7
+(define (make-interval a b) (cons a b))
+(define (lower-bound interval) (car interval))
+(define (upper-bound interval) (cdr interval))
+
+(define (add-interval x y)
+ (make-interval (+ (lower-bound x) (lower-bound y))
+                (+ (upper-bound x) (upper-bound y))))
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+;Exercise 2.10
+(define (div-interval x y)
+  (let ((up (upper-bound y))
+        (low (lower-bound y)))
+    (if (>= 0 (* up low))
+        (error "Unable to divide. Interval spans zero.")
+        (mul-interval x
+                      (make-interval (/ 1.0 (upper-bound y))
+                                     (/ 1.0 (lower-bound y)))))))
+; Exercise 2.8
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+; Exercise 2.9
+(define (width-interval interval)
+  (/ (- (upper-bound interval) (lower-bound interval)) 2))
+;;ex. this is evaluated to 6. (= 2 + 4)
+(width-interval (add-interval (make-interval 1 5) (make-interval 5 13)))
+;;ex. this is to 5. (= 3 + 2)
+(width-interval (sub-interval (make-interval 6 12) (make-interval 10 14)))
+;;ex. this is to 54. (/= 3 * 2)
+(width-interval (mul-interval (make-interval 6 12) (make-interval 10 14)))
+
 
 

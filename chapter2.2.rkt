2a12213ffeeec3ed1afebd0d744c9bf84855f39f
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2019-02-25 22:52:53 +0900

    [add]2.34: horner-eval

--- chapter2.2.rkt
+++ chapter2.2.rkt
@@ -264,4 +264,14 @@
 (define (length sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
+; Exercise2.34
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+
+
+
+
 

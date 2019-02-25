3c95787c4ebad179717592ba762b02a41a443341
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2019-02-25 23:02:17 +0900

    [add]2.36

--- chapter2.2.rkt
+++ chapter2.2.rkt
@@ -276,6 +276,12 @@
               0
               (enumerate-tree t)))
 
+; Exercise 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
 
 
 

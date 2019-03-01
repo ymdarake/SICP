87c3f73cf87ef4aaf0f8486767eed0868f7a1f1c
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2019-03-01 23:59:15 +0900

    [add]flatmap

--- chapter2.2.rkt
+++ chapter2.2.rkt
@@ -315,3 +315,31 @@
 
 
 
+;;; Nested Mappings
+
+(accumulate append
+            '()
+            (map (lambda (i)
+                   (map (lambda (j) (list i j))
+                        (enumerate-interval 1 (- i 1))))
+                 (enumerate-interval 1 5)))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
+
+
+

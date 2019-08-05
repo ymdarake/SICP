f5d79bb65cb06a4365ff8161b329abaf5300e6dc
ymdarake <t.sakamoto.ouen@gmail.com>
2019-08-05 11:10:33 +0900

    [add]3.3.2: 3.21, 3.22: Queue

---
+++ chapter3.3.2-queue.rkt
@@ -0,0 +1,100 @@
+#lang racket
+
+(define (cons x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+          ((eq? m 'cdr) y)
+          ((eq? m 'set-car!) set-x!)
+          ((eq? m 'set-cdr!) set-y!)
+          (else (error "Undefined operation: CONS" m))))
+  dispatch)
+
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+(define (set-car! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+(define (set-cdr! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
+
+
+
+;;; Queue
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (make-queue) (cons '() '()))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue) (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
+
+
+; Exercise 3.21
+(define (print-queue queue)
+  (define (iter x)
+    (if (null? x)
+        (newline)
+        (begin
+          (display (car x))
+          (iter (cdr x)))))
+  (iter (front-ptr queue)))
+
+; Exercise 3.22
+(define (make-queue-with-local-state)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (empty-queue?) (null? front-ptr))
+    (define (set-front-ptr! item) (set! front-ptr item))
+    (define (set-rear-ptr! item) (set! rear-ptr item))
+    (define (front-queue)
+      (if (empty-queue?)
+          (error "FRONT called with an empty queue")
+          (car front-ptr)))
+    (define (insert-queue! item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue?)
+               (set-front-ptr! new-pair)
+               (set-rear-ptr! new-pair))
+              (else
+               (set-cdr! rear-ptr new-pair)
+               (set-rear-ptr! new-pair)))))
+    (define (delete-queue!)
+      (cond ((empty-queue?) (error "DELETE called with an empty queue"))
+            (else (set-front-ptr! (cdr front-ptr)))))
+    (define (print-queue) front-ptr)
+    (define (dispatch m)
+      (cond ((eq? m 'empty-queue?) (empty-queue?))
+            ((eq? m 'front-queue) (front-queue))
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) (delete-queue!))
+            ((eq? m 'print-queue) (print-queue))
+            (else (error "undefined operation -- QUEUE" m))))
+    dispatch))
--- chapter3.3.rkt
+++ chapter3.3.rkt
@@ -2,18 +2,66 @@
 
 ; 3.3
 
-
-(define (cons x y)
-  (let ((new (get-new-pair)))
-    (set-car! new x)
-    (set-cdr! new y)
-    new))
+;(define (cons x y)
+;  (let ((new (get-new-pair)))
+;    (set-car! new x)
+;    (set-cdr! new y)
+;    new))
 
 ; Exercise 3.12
 (define (append x y)
   (if (null? x)
       y
       (cons (car x) (append (cdr x) y))))
-(define (append! x y)
-  (set-cdr! (last-pair x) y)
-  x)
+
+;(define (append! x y)
+;  (set-cdr! (last-pair x) y)
+;  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+; Exercise 3.13
+; mystery does "reverse".
+;(define (mystery x)
+;  (define (loop x y)
+;    (if (null? x)
+;        y
+;        (let ((temp (cdr x)))
+;          (set-cdr! x y)
+;          (loop temp x))))
+;  (loop x '()))
+
+
+; Exercise 3.16
+(define (count-pairs x)
+  (display x)
+  (newline)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+         (count-pairs (cdr x))
+         1)))
+(define x '(foo))
+(define y (cons x x))
+(define str3 (cons y y))
+(count-pairs str3)
+;(define str4 '(foo bar baz))
+;(set-cdr! (cddr str4) str4)
+;(count-pairs str4)
+
+; Exercise 3.17
+(define (count-pairs-3-17 x) 
+  (let ((encountered '())) 
+    (define (helper x) 
+      (if (or (not (pair? x)) (memq x encountered)) 
+          0 
+          (begin 
+            (set! encountered (cons x encountered)) 
+            (+ (helper (car x)) 
+               (helper (cdr x)) 
+               1)))) 
+    (helper x))) 
+

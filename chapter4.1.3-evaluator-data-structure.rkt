affcd2928dd2267fc1a9e1f99fef6cd1d028b6c0
ymdarake <t.sakamoto.ouen@gmail.com>
2019-08-21 17:21:23 +0900

    [add]4.13:

--- chapter4.1.3-evaluator-data-structure.rkt
+++ chapter4.1.3-evaluator-data-structure.rkt
@@ -126,3 +126,28 @@
         (add-binding-to-frame! var val frame))))
 
 (define f (make-frame '(a b c) '(3 4 5)))
+
+; Exercise 4.12
+
+; Exercise 4.13
+;; unbound variable in current frame 
+(define (unbound? expr) (tagged-list? expr 'unbound)) 
+(define (unbind-variable expr env) (make-unbound (cadr expr) env)) 
+(define (make-unbound variable env) 
+  (let ((vars (frame-variables (first-frame env))) 
+        (vals (frame-values (first-frame env)))) 
+    (define (unbound vars vals new-vars new-vals) 
+      (cond ((null? vars) 
+             (error "variable is not in the environment -- MAKE-UNBOUND"      
+  
+                    variable)) 
+            ((eq? (car vars) variable) 
+             (set-car! env  
+                       (cons (append new-vars (cdr vars))  
+                             (append new-vals (cdr vals))))) 
+            (else (unbound (cdr vars) (cdr vals)  
+                           (cons (car vars) new-vars)  
+                           (cons (car vals) new-vals))))) 
+    (unbound vars vals '() '()))) 
+  
+;; add this in eval 

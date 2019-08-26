a99977f6891655cf19d783f6e89b3d92d220a823
ymdarake <t.sakamoto.ouen@gmail.com>
2019-08-26 18:27:20 +0900

    [add]4.2: thunk

---
+++ chapter4.2.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
+(define (delay-it exp env)
+  (list 'thunk exp env))
+(define (thunk? obj) (tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+
+; non memoised version
+;(define (force-it obj)
+;  (if (thunk? obj)
+;      (actual-value (thunk-exp obj)
+;                    (thunk-env obj))
+;      obj))
+
+; memoised version
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk)
+  (cadr evaluated-thunk))
+
+(define (force-it obj)
+  (cond ((thunk? obj) (let ((result (actual-value (thunk-exp obj) (thunk-env obj))))
+                        (set-car! obj 'evaluated-thunk)
+                        (set-car! (cdr obj) result); replace exp with its value
+                        (set-cdr! (cdr obj) '());  ; forget unneeded env
+                        result))
+        ((evaluated-thunk? obj) (thunk-value obj))
+        (else obj)))
